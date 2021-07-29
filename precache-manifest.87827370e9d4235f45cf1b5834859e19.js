394beb95e25fa3439ab70f31d81ec02ce799dcbb
khoa nguyen <kenkinsai@gmail.com>
2021-07-29 23:40:58 +0700

    Updates

---
+++ precache-manifest.87827370e9d4235f45cf1b5834859e19.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71ac5865ce417a46d72a2f8e26588ba4",
+    "url": "/neta-web/index.html"
+  },
+  {
+    "revision": "4ec9e9795e614eca3a0d",
+    "url": "/neta-web/static/css/main.a41d2f60.chunk.css"
+  },
+  {
+    "revision": "4ef9771bead157001fba",
+    "url": "/neta-web/static/js/2.78642daa.chunk.js"
+  },
+  {
+    "revision": "07ecd83da62f715335e8ab633e4e8841",
+    "url": "/neta-web/static/js/2.78642daa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ec9e9795e614eca3a0d",
+    "url": "/neta-web/static/js/main.4ffd53f8.chunk.js"
+  },
+  {
+    "revision": "f1b44e50fd4ba32fa76b8e41776a09ea",
+    "url": "/neta-web/static/js/main.4ffd53f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98081714e3a19dd6f761",
+    "url": "/neta-web/static/js/runtime-main.b12fa5ce.js"
+  }
+]);
