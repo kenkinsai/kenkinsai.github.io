e2b35832eef55bbb24897876e77fc4516c95b924
khoa nguyen <kenkinsai@gmail.com>
2021-07-29 23:12:23 +0700

    Updates

---
+++ precache-manifest.92272cce374249159dc28fc246365e1f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2c65d00adae4f7f13edc3e3c93801d2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c501a4af41fe52e3eeb9",
+    "url": "/static/css/main.a41d2f60.chunk.css"
+  },
+  {
+    "revision": "99c73560092ba80da901",
+    "url": "/static/js/2.038dca61.chunk.js"
+  },
+  {
+    "revision": "07ecd83da62f715335e8ab633e4e8841",
+    "url": "/static/js/2.038dca61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c501a4af41fe52e3eeb9",
+    "url": "/static/js/main.4460143a.chunk.js"
+  },
+  {
+    "revision": "f1b44e50fd4ba32fa76b8e41776a09ea",
+    "url": "/static/js/main.4460143a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "360fe29fd8613d22744c",
+    "url": "/static/js/runtime-main.f0334303.js"
+  }
+]);
